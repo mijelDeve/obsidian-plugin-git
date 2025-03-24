0f251294afc2bbf247f861f6bcfc802a521026eb
Miguel Castillo <miguel.castillo@ibit.pe>
2025-03-24 07:54:17 -0500

    feat: haciendo ejemplo básico

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 import { Plugin } from 'obsidian';
 
 export default class ExamplePlugin extends Plugin {
-	onload(): void {
+	statusBarTextElement: HTMLSpanElement;
+
+	async onload(): Promise<void> {
+		this.statusBarTextElement = this.addStatusBarItem().createEl("span");
+		this.statusBarTextElement.textContent = "Hello, miguel";
 		console.log("Hello world!")
 	}
 }
